eb729a9676184988287200e370f1cdd236cc08d2
Keval1998 <git@stackblitz.com>
2025-11-09 21:44:20 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: 'flex',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'Inter, system-ui, sans-serif',
+        fontSize: '1.6rem',
+      }}
+    >
+      <div>
+        <h1>Hello World 🚀</h1>
+        <p style={{ color: '#555' }}>My first Next.js app</p>
+      </div>
+    </main>
+  );
+}
